597e02a44058d6fa4f164968c6f1a351975e9912
Aymen <76121581+aymks@users.noreply.github.com>
2022-06-17 23:51:22 +0100

    :100:

---
+++ Guess the number/home.js
@@ -0,0 +1,49 @@
+
+
+window.onload = function ()
+{
+    document.getElementById("submit").addEventListener("click",run);
+    document.getElementById("Reset").addEventListener("click",reset);
+}
+
+let tries=0;
+let num=Math.floor(Math.random() * 100);
+console.log(num);
+function run()
+{
+    
+    let numberGuess = document.getElementById('guess').value;
+    if(numberGuess>num)
+    {
+        document.getElementById("tess").innerHTML="too hight<br>"+document.getElementById("tess").innerHTML;
+        tries+=1;
+    }
+    else if (numberGuess<num)
+    {
+        document.getElementById("tess").innerHTML="too low<br>"+document.getElementById("tess").innerHTML;
+        tries+=1;
+    }
+    else
+    {
+        if(tries==0){
+            document.getElementById("tes").innerHTML="Wow it's "+num+"<br>"+"(1) try";
+        }
+        else{
+            document.getElementById("tes").innerHTML="You are Right it's "+num+"<br>"+"("+(tries+1)+") tries";
+        }
+        
+        
+    }
+   
+        document.getElementById('guess').value='';
+    
+}
+
+function reset()
+{
+    tries=0;
+    num = Math.floor(Math.random() * 100);
+    console.log(num);
+    document.getElementById("tess").innerHTML=" ";
+    document.getElementById("tes").innerHTML=" ";
+}
